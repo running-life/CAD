255d6da96a2a9ee5eddd9ac27d01dfba85c3df6b
baorunchen <baorun.chen01@gmail.com>
2023-11-16 15:54:55 +0800

    add ear-cut

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@
 #include "Eigen/Core"
 #include "stb_image.h"
 #include "EulerOperationTest.h"
+#include "earcut.h"
 
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
@@ -104,7 +105,34 @@ int main1()
 }
 
 int main() {
-    constructCube();
+    // constructCube();
+    // The number type to use for tessellation
+    using Coord = double;
+
+    // The index type. Defaults to uint32_t, but you can also pass uint16_t if you know that your
+    // data won't have more than 65536 vertices.
+    using N = uint32_t;
+
+    // Create array
+    using Point = std::array<Coord, 2>;
+    std::vector<std::vector<Point>> polygon;
+
+    // Fill polygon structure with actual data. Any winding order works.
+    // The first polyline defines the main polygon.
+    polygon.push_back({ {100, 0}, {100, 100}, {0, 100}, {0, 0} });
+    // Following polylines define holes.
+    polygon.push_back({ {75, 25}, {75, 75}, {25, 75}, {25, 25} });
+
+    // Run tessellation
+    // Returns array of indices that refer to the vertices of the input polygon.
+    // e.g: the index 6 would refer to {25, 75} in this example.
+    // Three subsequent indices form a triangle. Output triangles are clockwise.
+    std::vector<N> indices = mapbox::earcut<N>(polygon);
+
+    for (int i = 0; i < indices.size(); ++i) {
+        std::cout << indices[i] << " ";
+    }
+
 }
 
 
---
+++ src/visual/testVisual.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "earcut.h"
+#include "EulerOperationTest.h"
+
+void visual() {
+	Solid* cube = constructCube();
+	using Coord = double;
+	using N = int;
+	using Point = std::array < Coord, 2>;
+
+
+
+}
