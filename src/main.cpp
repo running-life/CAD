3b02736409bd264975823b18137a7f5de9a9b705
baorunchen <baorun.chen01@gmail.com>
2023-11-16 14:36:30 +0800

    set the environment of glut, but don't work properly

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
-#define GLEW_STATIC
+﻿#define GLEW_STATIC
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <GL/glut.h>
 
 #include <iostream>
 #include <stdio.h>
@@ -103,9 +104,33 @@ int main1()
     return 0;
 }
 
-int main() {
+int main2() {
     constructCube();
+    return 0;
 }
+
+
+void myDisplay()
+{
+    glClear(GL_COLOR_BUFFER_BIT);//清空色彩缓存区
+    glRectf(-0.5f, -0.5f, 0.5f, 0.5f);//绘制
+    glFlush();
+
+}
+
+
+int main(int argc, char* argv[])
+{
+    glutInit(&argc, argv);//初始化glut  （可不写默认）
+    glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);//显示模式初始化 RGB显示模式，单缓冲模式
+    glutInitWindowPosition(100, 100);//窗口窗口位置  （可不写默认）
+    glutInitWindowSize(400, 400);//窗口大小  （可不写默认）
+    glutCreateWindow("第一个OpenGl程序");//窗口名称
+    glutDisplayFunc(&myDisplay);//自定义函数绘图
+    glutMainLoop();//消息循环相应
+    return 0;
+}
+
 
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height){
